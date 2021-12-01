84e9e4e3b797a617ae5f7e0c629afa5741f3791d
Marton Cserbak <marton.cserbak@airtame.com>
2021-12-01 08:56:04 +0100

    Day 1 part 2 solution

--- day1/day1.py
+++ day1/day1.py
@@ -1,11 +1,30 @@
-prev_depth = None
-depth_increased = 0
+import functools
 
-with open("day1/input") as input:
-    for line in input:
-        curr_depth = int(line)
-        if prev_depth is not None and curr_depth > prev_depth:
-            depth_increased += 1
-        prev_depth = curr_depth
+def read_measurements():
+    with open("day1/input") as input:
+        return [ int(line) for line in input.readlines()]
 
-print(depth_increased)
+measurements = read_measurements()
+
+def count_increases(lst):
+    def reduce_fn(prev, curr):
+        (prev_head, increases) = prev
+        return (curr, increases + 1 if prev_head is not None and curr > prev_head else increases)
+
+    (_, increases) = functools.reduce(reduce_fn, lst, (None, 0))
+    return increases
+
+
+
+def part1():
+    return count_increases(measurements)
+
+def part2():
+    def sliding_windows_of_3(list):
+        for index in range(0, len(list)):
+            yield list[index : index + 3]
+
+    return count_increases([ sum(sw) for sw in sliding_windows_of_3(measurements)])
+
+print(f"Part1: {part1()}")
+print(f"Part2: {part2()}")

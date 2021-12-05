bbd75a09d94091f5c25fa532ca8e1c564a69db3b
Marton Cserbak <marton.cserbak@airtame.com>
2021-12-05 08:15:38 +0100

    Day5 part2

--- day5/day5.py
+++ day5/day5.py
@@ -7,12 +7,23 @@ def parse_line(line):
     return [ parse_point(pt) for pt in line.split(" -> ") ]
 
 def parse_input():
-    with open("day5/sample") as input:
+    with open("day5/input") as input:
         return [ parse_line(line) for line in input.readlines()]
 
 def expand_non_diagonal_line(line):
     (x1, y1), (x2, y2) = line
     return [ (x, y) for x in range(min(x1, x2), max(x1, x2)+1) for y in range(min(y1, y2), max(y1, y2)+1) ]
+
+def expand_diagonal_line(line):
+    (x1, y1), (x2, y2) = line
+    slope = int((x1 - x2) / (y1 - y2))
+    assert slope == 1 or slope == -1
+    intercept = y1 - slope * x1
+    return [ (x, y)
+        for x in range(min(x1, x2), max(x1, x2)+1)
+        for y in range(min(y1, y2), max(y1, y2)+1)
+        if y == slope * x + intercept
+    ]
 
 def count_occurrences(lst):
     return Counter(lst)
@@ -43,4 +54,23 @@ def part1():
 
     return len(overlapping_pts)
 
-print(f"Part1: {part1()}")
+#print(f"Part1: {part1()}")
+
+def part2():
+    lines = parse_input()
+
+    line_points = []
+    for line in lines:
+        line_points += (
+            expand_non_diagonal_line(line)
+            if horizontal(line) or vertical(line)
+            else expand_diagonal_line(line)
+        )
+
+    point_passes = count_occurrences(line_points)
+
+    overlapping_pts = overlapping_points(point_passes)
+
+    return len(overlapping_pts)
+
+print(f"Part2: {part2()}")

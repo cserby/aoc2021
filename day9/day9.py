d3c75b0b71e67be77547281cae677d0edef6b880
Marton Cserbak <marton.cserbak@airtame.com>
2021-12-09 15:15:26 +0100

    Day9 part2

--- day9/day9.py
+++ day9/day9.py
@@ -1,3 +1,5 @@
+from math import prod
+
 def parse_input():
     with open("day9/input") as input:
         return [[ int(digit) for digit in list(line.strip())] for line in input.readlines() ]
@@ -14,10 +16,20 @@ def cell(matrix, x, y):
     return safe_list_get(safe_list_get(matrix, x, []), y, None)
 
 def neighbors(matrix, x, y):
-    return [ c for c in [ cell(matrix, x-1, y), cell(matrix, x+1, y), cell(matrix, x, y-1), cell(matrix, x, y+1)] if c is not None ]
+    if x > 0:
+        yield (x - 1, y)
+    if x < len(matrix) - 1:
+        yield (x + 1, y)
+    if y > 0:
+        yield (x, y - 1)
+    if y < len(matrix[0]) - 1:
+        yield (x, y + 1)
+
+def neighbor_values(matrix, x, y):
+    return [ cell(matrix, x, y) for (x, y) in neighbors(matrix, x, y) ]
 
 def local_minimum(matrix, x, y):
-    return all(cell(matrix, x, y) < n for n in neighbors(matrix, x, y))
+    return all(cell(matrix, x, y) < n for n in neighbor_values(matrix, x, y))
 
 def local_minimums(matrix):
     for x in range(len(matrix)):
@@ -30,6 +42,44 @@ def local_minimum_risks(matrix):
 
 def part1():
     matrix = parse_input()
-    print(f"Part1: {sum(local_minimum_risks(matrix))}")
+    return sum(local_minimum_risks(matrix))
 
-part1()
+print(f"Part1: {part1()}")
+
+def write_cell(matrix, x, y, value):
+    matrix[x][y] = value
+    return matrix
+
+def basin_sizes(matrix):
+    visited = [[ False for _ in range(len(matrix[0]))] for _ in range(len(matrix)) ]
+
+    def unvisited():
+        for x in range(len(visited)):
+            for y in range(len(visited[0])):
+                if not cell(visited, x, y):
+                    yield (x, y)
+
+    def basin_size(matrix, x, y):
+        need_to_go = {(x, y)}
+        basin_size = 0
+        while len(need_to_go) > 0:
+            (visiting_x, visiting_y) = need_to_go.pop()
+            if cell(visited, visiting_x, visiting_y):
+                continue
+            else:
+                write_cell(visited, visiting_x, visiting_y, True)
+                if cell(matrix, visiting_x, visiting_y) == 9:
+                    continue
+                else:
+                    basin_size += 1
+                    need_to_go = need_to_go | set(neighbors(matrix, visiting_x, visiting_y))
+        return basin_size
+
+    for (x, y) in unvisited():
+        yield basin_size(matrix, x, y)
+
+def part2():
+    matrix = parse_input()
+    return prod(sorted(list(basin_sizes(matrix)), reverse=True)[:3])
+
+print(f"Basin size: {part2()}")

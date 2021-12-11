10c03fa9b67aec5504a48d1300ca4a9341a08445
Marton Cserbak <marton.cserbak@airtame.com>
2021-12-11 11:00:09 +0100

    Day11 part2

--- day11/day11.py
+++ day11/day11.py
@@ -60,20 +60,32 @@ def matrix_to_str(matrix):
 
     return matrix_str
 
-def after_step(matrix, n):
-    return reduce(lambda prev, _: evolve(matrix), range(n), matrix)
+def part1():
+    matrix_0 = parse_input()
+    print(f"Step#0: {matrix_0}")
+    matrix_prev = matrix_0
+    flash_sum = 0
+    for step in range(1, 100 + 1):
+        print(f"Step#{step}:\n")
+        matrix_step, flashed_step = evolve(matrix_prev)
+        flash_count = count_flashes(flashed_step)
+        print(f"Matrix:\n{matrix_to_str(matrix_step)}Flashed:\n{matrix_to_str(flashed_step)}Flash count: {flash_count}")
+        flash_sum += flash_count
+        matrix_prev = matrix_step
+    return flash_sum
 
-matrix_0 = parse_input()
+print(f"Part1: {part1()}")
 
-print(f"Step#0: {matrix_0}")
-matrix_prev = matrix_0
-flash_sum = 0
-for step in range(1, 100 + 1):
-    print(f"Step#{step}:\n")
-    matrix_step, flashed_step = evolve(matrix_prev)
-    flash_count = count_flashes(flashed_step)
-    print(f"Matrix:\n{matrix_to_str(matrix_step)}Flashed:\n{matrix_to_str(flashed_step)}Flash count: {flash_count}")
-    flash_sum += flash_count
-    matrix_prev = matrix_step
+def part2():
+    matrix_prev = parse_input()
+    flash_count = 0
+    step_count = 0
+    while flash_count != len(matrix_prev) * len(matrix_prev[0]):
+        step_count += 1
+        matrix_step, flashed_step = evolve(matrix_prev)
+        flash_count = count_flashes(flashed_step)
+        print(f"Step #{step_count}, flash_count: {flash_count}")
+        matrix_prev = matrix_step
+    return step_count
 
-print(f"Flashes after day 100: {flash_sum}")
+print(f"Part2: {part2()}")

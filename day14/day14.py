67c20204479ac6a593f46a13f4d41285d8b3e6e0
Marton Cserbak <marton.cserbak@airtame.com>
2021-12-14 19:45:42 +0100

    Day14 part2 - phew

--- day14/day14.py
+++ day14/day14.py
@@ -1,5 +1,5 @@
-from itertools import groupby
 from functools import reduce
+from itertools import groupby
 from typing import Counter
 
 
@@ -38,9 +38,48 @@ def evolve(template, patterns):
 def after_step(step, template, patterns):
     return reduce(lambda prev, _: evolve(prev, patterns), range(step), template)
 
-def part1():
-    template, patterns = parse_input()
-    counter = Counter(after_step(10, template, patterns))
+def most_common_minus_least_common(counter):
     return max(counter.values()) - min(counter.values())
 
+def part1():
+    template, patterns = parse_input()
+    return most_common_minus_least_common(Counter(after_step(10, template, patterns)))
+
 print(f"Part1: {part1()}")
+
+def evolve_pairs(pair_counts, patterns):
+    new_pair_counts = {**pair_counts}
+    for pair in pair_counts.keys():
+        if pair in patterns.keys():
+            try:
+                new_pair_counts[pair[0] + patterns[pair]] += pair_counts[pair]
+            except KeyError:
+                new_pair_counts[pair[0] + patterns[pair]] = pair_counts[pair]
+            try:
+                new_pair_counts[patterns[pair] + pair[1]] += pair_counts[pair]
+            except KeyError:
+                new_pair_counts[patterns[pair] + pair[1]] = pair_counts[pair]
+            new_pair_counts[pair] -= pair_counts[pair]
+    return new_pair_counts
+
+def pairs_after_step(step, template, patterns):
+    return reduce(lambda prev, _: evolve_pairs(prev, patterns), range(step), Counter(pairs(template)))
+
+# To avoid duplication, for each pair it counts the first element
+# This'll leave unaccounted for the last element in the polymer!
+def count_elements_except_last(pair_counter):
+    element_counter = dict()
+    for pair in pair_counter.keys():
+        try:
+            element_counter[pair[0]] += pair_counter[pair]
+        except KeyError:
+            element_counter[pair[0]] = pair_counter[pair]
+    return element_counter
+
+def part2():
+    template, patterns = parse_input()
+    return most_common_minus_least_common({
+        # Compensate for the last element in the counts
+        element: (count if element != template[-1] else count + 1) for element, count in count_elements_except_last(pairs_after_step(40, template, patterns)).items()})
+
+print(f"Part2: {part2()}")

7c5cee41cfe8fa08c778be8e9d4cb3baee3dc58d
Marton Cserbak <marton.cserbak@airtame.com>
2021-12-15 17:21:51 +0100

    Day15 part2 optimized

--- day15/day15.py
+++ day15/day15.py
@@ -1,4 +1,4 @@
-#import cProfile
+import cProfile
 
 def parse_input():
     with open("day15/input") as input:
@@ -15,53 +15,37 @@ def neighbors(matrix, x, y):
         yield (x, y + 1)
 
 def minimal_risks_from_top_left(matrix):
-    prev = [[ None for _ in range(len(matrix[0]))] for _ in range(len(matrix)) ]
-    risks = [[ None for _ in range(len(matrix[0]))] for _ in range(len(matrix)) ]
+    risks = dict()
     not_visited = { (x, y) for x in range(len(matrix[0])) for y in range(len(matrix)) }
 
-    risks[0][0] = 0
+    risks[(0, 0)] = 0
+    frontier = { (0, 0) }
 
-    def is_unvisited(x, y):
-        return (x, y) in not_visited
-
-    def min_risk_unvisited():
-        min_risk_unvisited = None
-        min_risk = None
-        for (x, y) in not_visited:
-            if min_risk == None:
-                min_risk = risks[x][y]
-                min_risk_unvisited = (x, y)
-            elif risks[x][y] == None:
-                continue
-            elif min_risk > risks[x][y]:
-                min_risk = risks[x][y]
-                min_risk_unvisited = (x, y)
-            else:
-                continue
-        return min_risk_unvisited
+    def min_risk_on_frontier():
+        risks_unvisited = { coord: risks[coord] for coord in frontier }
+        return min(risks_unvisited, key=risks_unvisited.get)
 
     while len(not_visited) > 0:
-        (x, y) = min_risk_unvisited()
+        (x, y) = min_risk_on_frontier()
         not_visited.remove((x, y))
 
-        if prev[0][0] == None:
-            prev[0][0] = ( -1, -1 )
+        frontier.remove((x, y))
+        frontier = frontier | set(coord for coord in neighbors(matrix, x, y) if coord in not_visited)
 
         for (neighbor_x, neighbor_y) in neighbors(matrix, x, y):
-            if not is_unvisited(neighbor_x, neighbor_y):
+            if (neighbor_x, neighbor_y) not in not_visited:
                 continue
-            neighbor_risk = risks[neighbor_x][neighbor_y]
-            alt = risks[x][y] + matrix[neighbor_x][neighbor_y]
+            alt = risks[(x, y)] + matrix[neighbor_x][neighbor_y]
+            neighbor_risk = risks.get((neighbor_x, neighbor_y), None)
             if neighbor_risk is None or neighbor_risk > alt:
-                risks[neighbor_x][neighbor_y] = alt
-                prev[neighbor_x][neighbor_y] = (x, y)
+                risks[(neighbor_x, neighbor_y)] = alt
 
-    return risks, prev
+    return risks
 
 def part1():
     matrix = parse_input()
-    risks, prev = minimal_risks_from_top_left(matrix)
-    return risks[-1][-1]
+    risks = minimal_risks_from_top_left(matrix)
+    return risks[(len(matrix) - 1, len(matrix[-1]) - 1)]
 
 #cProfile.run('part1()')
 print(f"Part1: {part1()}")
@@ -80,10 +64,11 @@ def n_x_n_times(matrix, n):
     matrix_n_x_n = [ increase_line(matrix_n[index], instance) for instance in range(n) for index in range(len(matrix_n)) ]
     return matrix_n_x_n
 
-def part2():
+def part2(n = 5):
     matrix = parse_input()
-    matrix25 = n_x_n_times(matrix, 5)
-    risks, prev = minimal_risks_from_top_left(matrix25)
-    return risks[-1][-1]
+    matrix25 = n_x_n_times(matrix, n)
+    risks = minimal_risks_from_top_left(matrix25)
+    return risks[(len(matrix25) - 1, len(matrix25[-1]) - 1)]
 
 print(f"Part2: {part2()}")
+#cProfile.run('part2(2)')

ed355094fecc881a87f65e946f5611873279a1b2
Marton Cserbak <marton.cserbak@airtame.com>
2021-12-22 07:44:57 +0100

    Day22 part2 - brute force (OOMkill)

--- day22/day22.py
+++ day22/day22.py
@@ -1,6 +1,7 @@
-def parse_input(file = "day22/sample"):
+def parse_input(file="day22/sample"):
     with open(file) as f:
-        return [ parse_line(line) for line in f.readlines() ]
+        return [parse_line(line) for line in f.readlines()]
+
 
 def parse_line(line):
     (on_off, coords) = line.split()
@@ -12,23 +13,27 @@ def parse_line(line):
 
     return (on_off, (x_l, x_h), (y_l, y_h), (z_l, z_h))
 
+
 def parse_coord_range(coord_range):
-    (c_l, c_h) =  coord_range.split("=")[1].split("..")
+    (c_l, c_h) = coord_range.split("=")[1].split("..")
     return (int(c_l), int(c_h))
 
-def cuboids(x_r, y_r, z_r, limit = 50):
-    (x_l, x_h) = x_r
-    (y_l, y_h) = y_r
-    (z_l, z_h) = z_r
 
-    limit_l = -1 * limit
+def cuboids(x_r, y_r, z_r, limit=50):
+    def coord_range(c_r):
+        (c_l, c_h) = c_r
+        return range(
+            max(-1 * limit, c_l) if limit is not None else c_l,
+            min(limit, c_h) + 1 if limit is not None else c_h + 1
+        )
 
-    for x in range(max(limit_l, x_l), min(limit, x_h) + 1):
-        for y in range(max(limit_l, y_l), min(limit, y_h) + 1):
-            for z in range(max(limit_l, z_l), min(limit, z_h) + 1):
+    for x in coord_range(x_r):
+        for y in coord_range(y_r):
+            for z in coord_range(z_r):
                 yield (x, y, z)
 
-def reboot(instructions, limit = 50):
+
+def reboot(instructions, limit=50):
     on = set()
     for instruction in instructions:
         (on_off, x_r, y_r, z_r) = instruction
@@ -38,7 +43,14 @@ def reboot(instructions, limit = 50):
             on = on - set(cuboids(x_r, y_r, z_r, limit))
     return on
 
+
 def part1():
     return len(reboot(parse_input("day22/input")))
 
+
 print(f"Part1: {part1()}")
+
+def part2():
+    return len(reboot(parse_input("day22/sample"), limit=None))
+
+print(f"Part2: {part2()}")
